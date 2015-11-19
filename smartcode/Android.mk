487d1bbe26d6c84dc7ff5d47a70b7509fa05e249
alex.zhang <alex.zhang@alo7.com>
2015-11-19 16:47:10 +0800

    fix name

--- smartcode/Android.mk
+++ smartcode/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE    := libsmartcode
+LOCAL_MODULE    := libsmartcodecode
 LOCAL_SRC_FILES := smartcode.h
 
 include $(BUILD_SHARED_LIBRARY)
